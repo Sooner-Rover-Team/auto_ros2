13472f75cec8e01ca129ec980e94f5d4c4973d60
Tyler R <tyler73750@gmail.com>
2025-02-25 15:48:49 -0600

    The rover can be a chill guy

--- src/navigator_node/navigator_node/main.py
+++ src/navigator_node/navigator_node/main.py
@@ -1,36 +1,54 @@
 from enum import Enum
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from loguru import logger as llogger
 
-# RCLPY Imports
+# rclpy
+import rclpy
 from rclpy.node import Node
+from rclpy.parameter import Parameter
 from rclpy.publisher import Publisher
 from rclpy.subscription import Subscription
 from rclpy.client import Client
 from rclpy.timer import Timer
 
-# ROS Message Types
+# ROS/Custom Message Types
 from geographic_msgs.msg import GeoPointStamped, GeoPoint
 from geometry_msgs.msg import PoseStamped
-from sensor_msgs.msg import Imu  # TODO: Does this have to be custom?
+from sensor_msgs.msg import Imu
+from custom_interfaces.msg import WheelsMessage
+
+# how long we'll keep the data (DDS).
+QUEUE_SIZE: int = 10
 
 
 class RoverTask(Enum):
-    """Describes the current"""
+    """
+    Describes the current rover task:
+        - GNSS Coordinate
+        - ArUco Marker
+        - Object Detection
+    """
 
-    GNSS_COORD = 0
-    ARUCO_MARKER = 1
-    OBJECT_DETECTION = 2
+    STAY_STILL_BRO = 0  # For debug purposes
+    GNSS_COORD = 1
+    ARUCO_MARKER = 2
+    OBJECT_DETECTION = 3
 
 
 class NavigationMode(Enum):
     """Describes the current navigation mode of the rover."""
 
-    NAV_TO_GNSS = 0
-    CALC_ARUCO_MARKER_POSE = 1
+    STANDBY = 0
+    NAV_TO_GNSS = 1
+    CALC_ARUCO_MARKER_POSE = 2
 
 
 @dataclass(kw_only=True)
 class NavigatorNode(Node):
+    # ARGUMENTS
+    _rover_task = RoverTask
+    """The rover's current task."""
+
     # PUBLISHERS
     _wheels_publisher: Publisher
     """Publish wheel speeds to move the Rover."""
@@ -72,11 +90,98 @@ class NavigatorNode(Node):
 
     _goal_reached: bool = False
     """Whether or not the rover task has been completed."""
-    _coord_queue: list[GeoPoint] = []
+    _coord_queue: list[GeoPoint] = field(default_factory=list)
 
     def __init__(self):
         """Create a new `NavigatorNode`"""
-        super().__init__("Navigator Node")
+        super().__init__("navigator_node")
 
         # Get relevant parameters
+        self.declare_parameter("rover_task", Parameter.Type.STRING)
+        if not self.has_parameter("rover_task"):
+            raise ValueError("Required parameter `rover_task` is missing")
+        try:
+            self._rover_task = RoverTask[self.get_parameter("rover_task").value]
+        except KeyError:
+            raise KeyError(
+                f"The value of `rover_task`={self.get_parameter('rover_task').value} is not a valid rover task. Refer to `RoverTask` for all possible values."
+            )
+        llogger.info(
+            f"Preparing Navigator Node for the following task: {self._rover_task}"
+        )
+
+        # Create publishers
+        self._wheels_publisher = self.create_publisher(
+            msg_type=WheelsMessage,
+            topic="/controls/wheels",
+            qos_profile=QUEUE_SIZE,
+        )
+        llogger.info("Finished making publishers!")
+
+        # Create subscribers
+
+        # Create services
+
+        # Plot path to goal
+
+        # Start navigating
+        self._navigator_callback_timer = self.create_timer(0.5, self.navigator)
+        llogger.info("Starting navigation!")
+
+    def navigator(self):
+        """Send wheel speeds depending on current mode of the rover."""
+        llogger.debug("Navigating!")
+
+        # Ensure navigation parameters were specified
+        if self._rover_task is None:
+            llogger.error("No rover task was specified!")
+            self.destroy_node()
+            rclpy.shutdown()
+
+        # Check if goal was reached
+
+        # Ensure we've started receiving rover coordinates
+
+        # Ensure rover coordinates are updating
+
+        # Send wheel speeds depending on our current mode
+        wheel_speeds = WheelsMessage()
+        match self._rover_task:
+            case RoverTask.STAY_STILL_BRO:
+                wheel_speeds.left_wheels = 0
+                wheel_speeds.left_wheels = 0
+                llogger.info("The rover is just a chill guy")
+
+        self._wheels_publisher.publish(wheel_speeds)
+
+    def rover_coord_callback(self, msg: GeoPointStamped):
+        """Callback to set rover coordinate information."""
         pass
+
+    def marker_pose_callback(self, msg: PoseStamped):
+        """Callback to set marker pose information."""
+        pass
+
+    def rover_imu_callback(self, msg: Imu):
+        """Callback to set rover imu information."""
+        pass
+
+    def __hash__(self) -> int:
+        return super().__hash__()
+
+
+def main(args: list[str] | None = None):
+    """Spinup (start) the Navigator Node."""
+    rclpy.init(args=args)
+    navigator_node = NavigatorNode()
+
+    try:
+        rclpy.spin(navigator_node)
+    except Exception as e:
+        llogger.error(e)
+        navigator_node.destroy_node()
+        rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
